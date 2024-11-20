95ca882de1ab00131b4d77d6db3dc1f426402761
turly221 <feifan@scntist.com>
2024-11-20 14:37:40 +0000

    commit patch 5730710

--- coders/png.c
+++ coders/png.c
@@ -4246,6 +4246,8 @@ static Image *ReadOneJNGImage(MngInfo *mng_info,
             c;
 
           c=ReadBlobByte(image);
+          if (c == EOF)
+            break;
           chunk[i]=(unsigned char) c;
         }
 
@@ -5128,6 +5130,8 @@ static Image *ReadMNGImage(const ImageInfo *image_info,ExceptionInfo *exception)
                 c;
 
               c=ReadBlobByte(image);
+              if (c == EOF)
+                break;
               chunk[i]=(unsigned char) c;
             }
 
